45f6f672bd106000ec52ba5f7b8d12c2321ed9dc
Afrocodeur <d.mamadou@miridoo.net>
2025-11-03 13:38:43 +0000

    fix(export): Validator

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ export { NDElement } from './src/wrappers/NDElement';
 export { TemplateCloner, useCache } from './src/wrappers/TemplateCloner';
 export { SingletonView, useSingleton } from './src/wrappers/SingletonView';
 export { default as PluginsManager } from './src/utils/plugins-manager';
+import { default as Validator } from './src/utils/validator';
 
 import './src/utils/prototypes.js';
 
@@ -16,8 +17,7 @@ export * from './src/data/observable-helpers/object';
 export * from './src/data/observable-helpers/computed';
 export * from './src/data/Store';
 
-import * as Validator from './src/utils/validator'
 import * as elements from './elements';
 import * as router from './router';
 
-export { elements, router, Validator, };
+export { elements, router, };
