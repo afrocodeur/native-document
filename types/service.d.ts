2d83238a5bcd12474fd6c42450add670ce2a8bfd
Afrocodeur <d.mamadou@miridoo.net>
2025-12-11 05:15:27 +0000

    fix(types): correct Service proxy return types

--- types/service.d.ts
+++ types/service.d.ts
@@ -1,13 +1,23 @@
 type ServiceFactory<T> = () => T;
 type MemoizedFactory<T, Args extends any[]> = (...args: Args) => T;
 
-interface ServiceStatic {
+type OnceProxy<T> = {
+    [K in keyof T]: T[K];
+};
 
-    once<T>(fn: ServiceFactory<T>): ServiceFactory<T>;
+type MemoizeProxy<T> = {
+    [key: string]: T;
+    [key: symbol]: T;
+};
+
+interface ServiceStatic {
+    once<T extends object>(fn: ServiceFactory<T>): OnceProxy<T>;
+
+    memoize<T>(fn: () => T): MemoizeProxy<T>;
 
     memoize<T, Args extends any[]>(
-        fn: MemoizedFactory<T, Args>
-    ): MemoizedFactory<T, Args>;
+        fn: (...args: Args) => T
+    ): MemoizeProxy<(...args: Args) => T>;
 }
 
 export const Service: ServiceStatic;
